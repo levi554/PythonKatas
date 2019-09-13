5f2f0b8e796bd8a4c56ebd5e516454341c96c054
Jonathan David Livesay <jonl316@gmail.com>
2019-09-12 23:51:16 -0500

    Added several tests for the string_kata module

--- string_kata.py
+++ string_kata.py
@@ -1,4 +1,18 @@
 
 
-def string_test_1():
-    return 'One'
+def string_is_of_type_string(string):
+
+    return str(string)
+
+
+def reversed_string(string):
+
+    return string[::-1]
+
+
+def all_caps(string):
+    return str.upper(string)
+
+
+def all_lower_case(string):
+    return str.lower(string)
--- test/string_kata_test.py
+++ test/string_kata_test.py
@@ -2,6 +2,26 @@ from ..string_kata import *
 
 
 def test_test1_returns_string():
-    test1_string = string_test_1()
+    test1_string = string_is_of_type_string(1234)
 
     assert str(test1_string)
+
+
+def test_string_is_reversed():
+    reversed_str = reversed_string('Jonathan')
+
+    assert reversed_str == 'nahtanoJ'
+
+
+def test_string_is_all_caps():
+    all_caps_string = all_caps('this is all caps')
+
+    assert all_caps_string == 'THIS IS ALL CAPS'
+
+
+def test_string_is_all_lowercase():
+
+    lowercase = all_lower_case('JoNaThAn')
+
+    assert lowercase == 'jonathan'
+
