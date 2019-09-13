8cb75afc839c07b5788e7d169a29d09b56856ada
Jonathan David Livesay <jonl316@gmail.com>
2019-09-12 22:27:44 -0500

    INITIAL COMMIT

---
+++ string_kata.py
@@ -0,0 +1,4 @@
+
+
+def string_test_1():
+    return 'One'
---
+++ string_kata_test.py
@@ -0,0 +1,7 @@
+from string_kata import *
+
+
+def test_test1_returns_string():
+    test1_string = string_test_1()
+
+    assert str(test1_string)
